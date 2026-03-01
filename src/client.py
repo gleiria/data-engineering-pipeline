a7165bb9e2c9485784face649ad2c0cbf7d10c10
gleiria <g.n.leiria@exeter.ac.uk>
2026-03-01 15:32:36 +0000

    added __init__.py to src/ for python package

--- src/client.py
+++ src/client.py
@@ -4,22 +4,24 @@ import requests
 import yaml
 
 
-ENDPOINT = "https://api.openweathermap.org/data/2.5/weather" #endpoint
+ENDPOINT = "https://api.openweathermap.org/data/2.5/weather" 
 
 def load_yaml(path: str) -> dict:
-    """ Load a YAML file and return its contents as a dictionary. 
-        This is to load secrets and config files."""
+    """Parse content of yaml and return as dict"""
     with open(path, 'r') as f:
         return yaml.safe_load(f)
     
 def fetch_weather(city: str, api_key: str, units: str) -> dict:
-    """ Fetch weather data for a given city using OpenWeatherMap API"""
+    """Fetch weather data for a given city using OpenWeatherMap API"""
     params = {
         'q': city,
         'appid': api_key,
         'units': units
     }
+    # send GET request
     response = requests.get(ENDPOINT, params=params)
+    # check success or rise error
     response.raise_for_status()
+    # if successful, return json 
     return response.json()
 
