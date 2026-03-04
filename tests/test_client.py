c9467638eb17797c667e5fc169d0cdf13c97e6fe
gleiria <g.n.leiria@exeter.ac.uk>
2026-03-04 10:55:21 +0000

    tieded up test_clients.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,12 +4,13 @@ import pytest
 from unittest.mock import MagicMock
 from src.client import load_yaml, fetch_weather
 
-def test_load_yaml(tmp_path: Path):
+def test_load_yaml(tmp_path: Path): #tmp_path is a pytest fixture that provides a temporary directory for testing
     # arrange: Given a sample YAML file
     sample_yaml = """
     key1: value1
     key2: value2
     """
+    # write yaml to temporary file
     file = tmp_path / "test.yaml"
     file.write_text(sample_yaml)
 
@@ -21,8 +22,9 @@ def test_load_yaml(tmp_path: Path):
 
 
 
-def test_fetch_weather_success(monkeypatch):
+def test_fetch_weather_success(monkeypatch): # monkeypath to mock the API response from requests.get
     # arrange: Given a successful API response
+
     # create mock response object
     mock_response = MagicMock()
     mock_response.json.return_value = {
